83e03e676efca0c22e27b31c4c68dc3644d986f6
hawkobe <sanders.jms@gmail.com>
2023-03-22 16:28:17 -0600

    working on create_graph method

--- lib/knight.rb
+++ lib/knight.rb
@@ -24,24 +24,42 @@ class Knight
 
   # need a history of where the knight
   # has been so it doesn't get caught in a loop
-
-  def create_graph(start)
-    root = GraphVertex.new(start)
-
-    MOVES.each do |move|
-      if (1..8).include?(start[0] + move[0]) && (1..8).include?(start[1] + move[1])
-        root.add_edge([(start[0] + move[0]), (start[1] + move[1])])
-      end
+  
+  def is_valid?(x_position, y_position)
+    if x_position.between?(1, 8) && y_position.between?(1, 8) && !@move_history.include?([x_position, y_position])
+      true
+    else
+      false
     end
+  end
+
+  def create_graph(start_loc, end_loc)
+    node = GraphVertex.new(start_loc)
+
+    return if node.value == end_loc
+    
+    MOVES.each do |move|
+      if is_valid?(start_loc[0] + move[0], start_loc[1] + move[1])
+        node.add_edge([start_loc[0] + move[0], start_loc[1] + move[1]])
+        @graph.add_node([start_loc[0] + move[0], start_loc[1] + move[1]])
+        create_graph([start_loc[0] + move[0], start_loc[1] + move[1]], end_loc)
+      end
+
+      # if (1..8).include?(start_loc[0] + move[0]) && (1..8).include?(start_loc[1] + move[1])
+      #   node.add_edge([(start_loc[0] + move[0]), (start_loc[1] + move[1])])
+      # end
+    end
+
 
     # need to add a queue of some sort
 
-    @graph.add_node(root)
+    # @graph.add_node(root)
 
     p @graph.nodes
 
-    # root.neighbors.each do |neighbor|
-    #   create_graph(neighbor)
+    # node.neighbors.each do |neighbor|
+    #   return if neighbor == end_loc
+    #   create_graph(neighbor, end_loc)
     # end
   end
 
